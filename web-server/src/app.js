1817f662aff3e01c702847fc78f99cc4f5aeb3fb
Adrian Quevedo <adrianqvd@gmail.com>
2019-12-19 22:23:34 -0300

    add new files for the course

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,13 +4,36 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
+
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Adrian Quevedo'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather App',
+        name: 'Adrian Quevedo'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
         location: 'Tandil',
         temperature: 29
+    }) 
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        msg: 'Please contact with the creator of the page',
+        error: 404
     }) 
 })
 
