7fc7270b81522f348df0ba9cebaee5ac56384394
Adrian Quevedo <adrian.quevedo@sdc.com.ar>
2020-01-08 15:58:31 -0300

    Comienzo api rest

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,42 +5,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User',{
-    name: {
-        type: String
-    },
-    age: {
-        type:Number
-    }
-})
-
-// const me = new User({
-//     name: 'Adrian',
-//     age: 28
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error', error)
-// })
-
 const Task = mongoose.model('Task',{
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
-})
-
-const myTask = new Task({
-    description: "Clean the house",
-    completed: false
-})
-
-myTask.save().then(() => {
-    console.log(myTask)
-}).catch((error) => {
-    console.log('Error ', error)
 })
