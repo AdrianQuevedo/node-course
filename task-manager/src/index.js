3efefe684b5ddd82a704bb4b67aeaf23535e5723
Adrian Quevedo <adrian.quevedo@sdc.com.ar>
2020-01-15 11:31:15 -0300

    finish seccion sort, pagination

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -27,14 +27,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const main = async () => {
-    // const task = await Task.findById('5e1e6d80744f8a0264d221db')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-    const user = await User.findById('5e1e6d7b744f8a0264d221d9')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
