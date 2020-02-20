d3a1f1a98cbd4da0a4203f6b40dbc65f400b7d33
Adrian Quevedo <adrianqvd@gmail.com>
2020-02-19 23:37:54 -0300

    avances

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,27 +1,71 @@
 const socket = io()
 
-const chatForm = document.querySelector('form')
-const search = document.querySelector('input')
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
 
-chatForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
-})
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
+
+// Options
+const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const search = document.querySelector('input')
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate,{
+        message: message.text,
+        createdAt: moment(message.createdAt).format('H:MM')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html = Mustache.render(locationTemplate,{
+        url: url.url,
+        createdAt: moment(url.createdAt).format('H:MM')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message delivered')
+    })
+})
+
+$locationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
-
+    $locationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+            $locationButton.removeAttribute('disabled')
+            console.log('Location shared!')
         })
     })
 })
+
+socket.emit('join', {username, room})
